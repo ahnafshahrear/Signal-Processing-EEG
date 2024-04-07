bf05335b0ef45fbe2344d5d66b83d37e4f91e51f
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-04-07 19:44:07 +0600

    RippleAdder updated

--- RippleAdder.v
+++ RippleAdder.v
@@ -1,4 +1,5 @@
 module RippleAdder(sum,cout,a,b,cin);
+	
 	input [3:0]a,b;
 	input cin;
 	output [3:0]sum;
@@ -7,4 +8,5 @@ module RippleAdder(sum,cout,a,b,cin);
 	FullAdder fa2(sum[1],c2,a[1],b[1],c1);
 	FullAdder fa3(sum[2],c3,a[2],b[2],c2);
 	FullAdder fa4(sum[3],cout,a[3],b[3],c3);
+	
 endmodule

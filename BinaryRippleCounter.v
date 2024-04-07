fe1093978551c3f7e55d5a8e9a6685e0068d296a
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-04-07 19:29:18 +0600

    BinaryRippleCounter added

---
+++ BinaryRippleCounter.v
@@ -0,0 +1,11 @@
+module BinaryRippleCounter(num,clk,clear);
+	input clk,clear;
+	output [3:0]num;
+	wire j,k;
+	assign j=1;
+	assign k=1;
+	JkFlipFlop Jk1(num[0],qb1,j,k,clear,clk);
+	JkFlipFlop Jk2(num[1],qb2,j,k,clear,num[0]);
+	JkFlipFlop Jk3(num[2],qb3,j,k,clear,num[1]);
+	JkFlipFlop Jk4(num[3],qb4,j,k,clear,num[2]);
+endmodule
